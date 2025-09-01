76fa4592b0a379d92d003ee2dfd1d616cf23ebf9
karimk94 <karim.abdulwahed@gmail.com>
2025-09-01 13:58:16 +0400

    added cors

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from werkzeug.serving import run_simple
 
 # --- Initialization ---
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["http://127.0.0.1:5000", "http://127.0.0.1:5006","http://localhost:3000","http://127.0.0.1:5008" ]}})
+CORS(app, resources={r"/*": {"origins": ["http://127.0.0.1:5000", "http://127.0.0.1:5006","http://localhost:3000","http://127.0.0.1:5008", "http://192.168.146.198:5008" ]}})
 face_processor = FaceProcessor()
 
 # --- API Routes ---
